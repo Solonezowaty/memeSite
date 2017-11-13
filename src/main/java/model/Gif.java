600f543eb5c50a83c5a3c51afbfd74c5bc32eb04
JakubS <jakubsolo1103@gmail.com>
2017-11-13 16:02:39 +0100

    problem

--- src/main/java/model/Gif.java
+++ src/main/java/model/Gif.java
@@ -1,20 +1,5 @@
 package model;
 
-<<<<<<< HEAD
-public class Gif{
-    private String name;
-    private String username;
-    private boolean favourite;
-
-    public Gif(String name, String username) {
-        this.name = name;
-        this.username = username;
-=======
-import memeSite.dao.GifDao;
-
-import java.util.ArrayList;
-import java.util.List;
-
 public class Gif {
 
     String name;
@@ -23,7 +8,6 @@ public class Gif {
     public Gif(String name, String username) {
         this.name=name;
         this.username=username;
->>>>>>> 7ac1bece73cb00b702770ebc0ceb5c92170b91cd
     }
 
     public String getName() {
@@ -32,24 +16,6 @@ public class Gif {
 
     public void setName(String name) {
         this.name = name;
-<<<<<<< HEAD
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public boolean isFavourite() {
-        return favourite;
-    }
-
-    public void setFavourite(boolean favourite) {
-        this.favourite = favourite;
-=======
     }
 
     public String getUsername() {
@@ -62,7 +28,6 @@ public class Gif {
 
     public String getUrl(){
         return getName()+".gif";
->>>>>>> 7ac1bece73cb00b702770ebc0ceb5c92170b91cd
     }
     // for (File file : files) {
       //  if (file.isFile()) {
