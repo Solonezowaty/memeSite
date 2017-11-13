cd8fa1b65ccac1c51f56ffab82600d0daa5b843c
JakubS <jakubsolo1103@gmail.com>
2017-11-13 13:07:49 +0100

    first

---
+++ src/main/java/memeSite/memeSite/MemeSiteApplication.java
@@ -0,0 +1,12 @@
+package memeSite.memeSite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemeSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemeSiteApplication.class, args);
+	}
+}
